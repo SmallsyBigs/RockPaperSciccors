14ee3f6a7a3d01ac86786d14ff33f0975e1393f9
SmallsyBigs <bobionekenobi241@gmail.com>
2024-03-19 18:26:13 +0100

    add general pseudocode

---
+++ index.js
@@ -0,0 +1,5 @@
+// Make a function that returns at random Rock Paper or Scissors - it will be the computer's 'choice'
+// Make a variable with human's choice od either Rock Paper or Scissors
+// Make a function that plays a round of the game and returns the result
+
+// Make a new function that uses the previous function and variable to play the game and keep score
