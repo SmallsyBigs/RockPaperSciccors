783254f8a59671cc1aeff95333655cd2c5fd4d89
SmallsyBigs <bobionekenobi241@gmail.com>
2024-03-23 10:35:03 +0100

    delete redundant variable

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
-// Make a function that returns at random Rock Paper or Scissors - it will be the computer's 'choice'
 const choices = ["Rock", "Paper", "Scissors"];
-let playerChoice = null
+
+// Make a function that returns at random Rock Paper or Scissors - it will be the computer's 'choice'
 
 function getComputerChoice(){
+
     return choices[Math.floor(Math.random() * 3)];
+
 }
 
 console.log(getComputerChoice());
 
 // Make a function that returns the player's choice
-
 function getPlayerChoice(choice){
+
     console.log(choice);
     return choice;
-}
 
+}
 
 // Make a function that plays a round of the game and returns the result
 
