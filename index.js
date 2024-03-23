e3a06bc39e29efbbe2dc35716b63771c87901de1
SmallsyBigs <bobionekenobi241@gmail.com>
2024-03-23 10:59:47 +0100

    make the duction count score

--- index.js
+++ index.js
@@ -2,19 +2,45 @@
 const choices = ["Rock", "Paper", "Scissors"];
 let computerChoice = null
 let playerChoice = null
+let computerResult = 0
+let playerResult = 0 
 
 // Make a function that returns computer's choice
 function getComputerChoice(){
     computerChoice = choices[Math.floor(Math.random() * 3)];
     console.log(computerChoice)
 }
-getComputerChoice()
 
-// Make a function that plays a round of the game and returns the result
+// Make a function that plays a round of the game and displays the winner
 function playGame(choice){
     playerChoice = choice
+    getComputerChoice()
     console.log(playerChoice);
     if (playerChoice === computerChoice){
         document.getElementById("result").textContent="It's a tie!"
+    }
+    else if(playerChoice == "Rock" && computerChoice == "Paper"){
+        document.getElementById("result").textContent="You Loose!"
+        let computerResult = computerResult + 1
+    }
+    else if(playerChoice == "Rock" && computerChoice == "Scissors"){
+        document.getElementById("result").textContent="You Win!"
+        let playerResult = playerResult + 1
+    }
+    else if(playerChoice == "Paper" && computerChoice == "Rock"){
+        document.getElementById("result").textContent="You Win!"
+        let playerResult = playerResult + 1
+    }
+    else if(playerChoice == "Paper" && computerChoice == "Scissors"){
+        document.getElementById("result").textContent="You Loose!"
+        let computerResult = computerResult + 1
+    }
+    else if(playerChoice == "Scissors" && computerChoice == "Rock"){
+        document.getElementById("result").textContent="You Loose!"
+        let computerResult = computerResult + 1
+    }
+    else if(playerChoice == "Scissors" && computerChoice == "Paper"){
+        document.getElementById("result").textContent="You Win!"
+        let playerResult = playerResult + 1
     }
 }
