f5f3274d826316c9d496a5d1e9f96b987b747baa
SmallsyBigs <bobionekenobi241@gmail.com>
2024-03-23 10:12:42 +0100

    add html elements

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 // Make a function that returns at random Rock Paper or Scissors - it will be the computer's 'choice'
-const choices = ["Rock", "Paper", "Scissors"]
+const choices = ["Rock", "Paper", "Scissors"];
 
 function getComputerChoice(){
-    return choices[Math.floor(Math.random() * 3)]
+    return choices[Math.floor(Math.random() * 3)];
 }
 
-console.log(getComputerChoice())
+console.log(getComputerChoice());
 
 
 // Make a variable with human's choice od either Rock Paper or Scissors
+
 // Make a function that plays a round of the game and returns the result
 
 // Make a new function that uses the previous function and variable to play the game and keep score
