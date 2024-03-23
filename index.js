2b0e4d720074b8799489e3c9074e269724f3a941
SmallsyBigs <bobionekenobi241@gmail.com>
2024-03-23 10:47:32 +0100

    fuse getplayer choice function with playgame function

--- index.js
+++ index.js
@@ -1,26 +1,20 @@
+// declare needen variables
 const choices = ["Rock", "Paper", "Scissors"];
 let computerChoice = null
 let playerChoice = null
 
-// Make a function that returns at random Rock Paper or Scissors - it will be the computer's 'choice'
-
+// Make a function that returns computer's choice
 function getComputerChoice(){
-
     computerChoice = choices[Math.floor(Math.random() * 3)];
     console.log(computerChoice)
 }
 getComputerChoice()
 
-// Make a function that returns the player's choice
-function getPlayerChoice(choice){
-
+// Make a function that plays a round of the game and returns the result
+function playGame(choice){
     playerChoice = choice
     console.log(playerChoice);
-
+    if (playerChoice === computerChoice){
+        document.getElementById("result").textContent="It's a tie!"
+    }
 }
-
-
-
-// Make a function that plays a round of the game and returns the result
-
-// Make a new function that uses the previous function and variable to play the game and keep score
