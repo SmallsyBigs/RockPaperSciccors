107419e8f508e055e0745f8ac7a6dcb17d059feb
SmallsyBigs <bobionekenobi241@gmail.com>
2024-03-23 10:41:04 +0100

    declare variables and change function

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
 const choices = ["Rock", "Paper", "Scissors"];
+let computerChoice = null
+let playerChoice = null
 
 // Make a function that returns at random Rock Paper or Scissors - it will be the computer's 'choice'
 
 function getComputerChoice(){
 
-    return choices[Math.floor(Math.random() * 3)];
-
+    computerChoice = choices[Math.floor(Math.random() * 3)];
+    console.log(computerChoice)
 }
-
-console.log(getComputerChoice());
+getComputerChoice()
 
 // Make a function that returns the player's choice
 function getPlayerChoice(choice){
 
-    console.log(choice);
-    return choice;
+    playerChoice = choice
+    console.log(playerChoice);
 
 }
+
+
 
 // Make a function that plays a round of the game and returns the result
 
