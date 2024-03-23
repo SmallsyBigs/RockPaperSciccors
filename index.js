2c4765d48326b252ee7dcdfdd59604504ac0f95d
SmallsyBigs <bobionekenobi241@gmail.com>
2024-03-23 10:34:00 +0100

    declare function that gets player's choice

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Make a function that returns at random Rock Paper or Scissors - it will be the computer's 'choice'
 const choices = ["Rock", "Paper", "Scissors"];
+let playerChoice = null
 
 function getComputerChoice(){
     return choices[Math.floor(Math.random() * 3)];
@@ -7,8 +8,13 @@ function getComputerChoice(){
 
 console.log(getComputerChoice());
 
+// Make a function that returns the player's choice
 
-// Make a variable with human's choice od either Rock Paper or Scissors
+function getPlayerChoice(choice){
+    console.log(choice);
+    return choice;
+}
+
 
 // Make a function that plays a round of the game and returns the result
 
