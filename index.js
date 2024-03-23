06fd1a1fc28ef00b9e020b0ebb6ada359ed31f65
SmallsyBigs <bobionekenobi241@gmail.com>
2024-03-23 11:09:54 +0100

    fix referencing bug

--- index.js
+++ index.js
@@ -1,46 +1,46 @@
 // declare needen variables
 const choices = ["Rock", "Paper", "Scissors"];
-let computerChoice = null
-let playerChoice = null
-let computerResult = 0
-let playerResult = 0 
+let computerChoice = null;
+let playerChoice = null;
+let computerResult = 0;
+let playerResult = 0 ;
 
 // Make a function that returns computer's choice
 function getComputerChoice(){
     computerChoice = choices[Math.floor(Math.random() * 3)];
-    console.log(computerChoice)
+    console.log(computerChoice);
 }
 
 // Make a function that plays a round of the game and displays the winner
 function playGame(choice){
-    playerChoice = choice
-    getComputerChoice()
+    playerChoice = choice;
+    getComputerChoice();
     console.log(playerChoice);
     if (playerChoice === computerChoice){
-        document.getElementById("result").textContent="It's a tie!"
+        document.getElementById("result").textContent="It's a tie!";
     }
     else if(playerChoice == "Rock" && computerChoice == "Paper"){
-        document.getElementById("result").textContent="You Loose!"
-        let computerResult = computerResult + 1
+        document.getElementById("result").textContent="You Loose!";
+        computerResult = computerResult + 1;
     }
     else if(playerChoice == "Rock" && computerChoice == "Scissors"){
-        document.getElementById("result").textContent="You Win!"
-        let playerResult = playerResult + 1
+        document.getElementById("result").textContent="You Win!";
+        playerResult = playerResult + 1;
     }
     else if(playerChoice == "Paper" && computerChoice == "Rock"){
-        document.getElementById("result").textContent="You Win!"
-        let playerResult = playerResult + 1
+        document.getElementById("result").textContent="You Win!";
+        playerResult = playerResult + 1;
     }
     else if(playerChoice == "Paper" && computerChoice == "Scissors"){
-        document.getElementById("result").textContent="You Loose!"
-        let computerResult = computerResult + 1
+        document.getElementById("result").textContent="You Loose!";
+        computerResult = computerResult + 1;
     }
     else if(playerChoice == "Scissors" && computerChoice == "Rock"){
-        document.getElementById("result").textContent="You Loose!"
-        let computerResult = computerResult + 1
+        document.getElementById("result").textContent="You Loose!";
+        computerResult = computerResult + 1;
     }
     else if(playerChoice == "Scissors" && computerChoice == "Paper"){
-        document.getElementById("result").textContent="You Win!"
-        let playerResult = playerResult + 1
+        document.getElementById("result").textContent="You Win!";
+        playerResult = playerResult + 1;
     }
 }
