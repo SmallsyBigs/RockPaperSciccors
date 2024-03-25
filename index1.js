4d747d6d357da90b5dbd0fd8ca6d72ea4f52547c
SmallsyBigs <bobionekenobi241@gmail.com>
2024-03-25 18:31:59 +0100

    add semicolons and provide better comments

--- index1.js
+++ index1.js
@@ -1,7 +1,7 @@
-let playerScore = 0
-let computerScore = 0 
+let playerScore = 0;
+let computerScore = 0;
 
-// The function get's player's choice, formats it and reacts if the input is wrong
+// The purpuse of the function is to get user's choice in the game
 function getPlayerChoice(){
     const lowerCaseInput = prompt("Type rock, paper, or scissors").toLowerCase();
     if (lowerCaseInput == "rock"){
@@ -18,54 +18,55 @@ function getPlayerChoice(){
     }
 }
 
-// The function generates and returns random computer's choice
+// We need a computer's choice, hence the function
 function getComputerChoice(){
-    const choices = ["rock", "paper", "scissors"]
+    const choices = ["rock", "paper", "scissors"];
     return computerChoice = choices[Math.floor(Math.random() * 3)];
 }
 
-// This function plays a single round of the game and logs as well as counts the score
+// The purpuse of the function is to play one round of the game
+// The game is played in console so it logs the information and keeps track of the score so we can declare winner later
 function playRound(playerChoice, computerChoice){
     if(playerChoice === computerChoice){
-        console.log(playerChoice)
-        console.log(computerChoice)
-        console.log("it's a tie!")
+        console.log(playerChoice);
+        console.log(computerChoice);
+        console.log("it's a tie!");
     }
     else if(playerChoice == "rock" && computerChoice == "scissors"){
-        console.log(playerChoice)
-        console.log(computerChoice)
+        console.log(playerChoice);
+        console.log(computerChoice);
         console.log("You win!");
         playerScore += 1;
     }
     else if(playerChoice == "scissors" && computerChoice == "paper"){
-        console.log(playerChoice)
-        console.log(computerChoice)
+        console.log(playerChoice);
+        console.log(computerChoice);
         console.log("You win!");
         playerScore += 1;
     }
     else {
-        console.log(playerChoice)
-        console.log(computerChoice)
+        console.log(playerChoice);
+        console.log(computerChoice);
         console.log("You loose!");
         computerScore += 1;
     }
 }
 
-//This function plays 5 rounds, logs the score and declare the winner
+//The function repeats the previous function 5 times so the winner can be declared
 function playGame(){
     for (let i = 0; i < 5; i++){
-    playRound(getPlayerChoice(), getComputerChoice())
+    playRound(getPlayerChoice(), getComputerChoice());
     }
-    console.log(playerScore)
-    console.log(computerScore)
+    console.log(playerScore);
+    console.log(computerScore);
     if(playerScore > computerScore){
-        console.log("You have won!!!")
+        console.log("You have won!!!");
     }
     else if(playerScore == computerScore){
-        console.log("it's a tie!!!")
+        console.log("it's a tie!!!");
     }
     else {
-        console.log("You have lost :(")
+        console.log("You have lost :(");
     }
 }
-playGame()
+playGame();
